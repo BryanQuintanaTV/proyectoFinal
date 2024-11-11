2287ce58f2e5f46f73d45853e6b938193089ae52
Gabtor1109 <104457841+Gabtor1109@users.noreply.github.com>
2024-11-10 22:34:48 -0600

    views ordenes imprementado

--- ordenes/views.py
+++ ordenes/views.py
@@ -17,18 +17,18 @@ def cargar_ordenes():
         return json.load(file)
 
 # Función para sobreescribir el archivo de usuarios.json
-def guardar_ordenes(usuarios):
+def guardar_ordenes(ordenes):
     with open(PATH_ORDENES, 'w') as file:
-        json.dump(usuarios, file, indent=2)
+        json.dump(ordenes, file, indent=2)
 
 # Function for http Requests
 def ordenes(request):
 
     ordenes = cargar_ordenes()
 
-    # Verificar si hay usuarios
+    # Verificar si hay ordenes
     if len(ordenes) == 0:
-        return JsonResponse({"mensaje": "No hay productos"}, safe=False, status=404)
+        return JsonResponse({"mensaje": "No hay Ordenes"}, safe=False, status=404)
 
     if request.method == 'GET':
         respuesta = JsonResponse(ordenes, safe=False, status=200)
@@ -36,7 +36,69 @@ def ordenes(request):
 
 
     if request.method == 'POST':
-        return JsonResponse({"mensaje":"No hay ordenes"}, status=400)
+        try:
+            ordenes = cargar_ordenes()
+            nueva_orden = json.loads(request.body.decode('utf-8'))
+            if nueva_orden['id'] == 0:
+                id = newIndex()
+                nueva_orden['id'] = id
+
+            ordenes.append(nueva_orden)
+            guardar_ordenes(ordenes)
+
+            return JsonResponse({"mensaje": "Orden Creada Correctamente"}, status=200)
+        except:
+            return JsonResponse({"mensaje" : "Hubo un error al agregar la Orden"}, safe=False, status=400)
+
 
     else:
         return JsonResponse({"mensaje":"Método No Permitido"}, status=400)
+
+def newIndex():
+    ordenes = cargar_ordenes()
+    higher_index = 0
+
+    for orden in ordenes:
+        if orden['id'] > higher_index:
+            higher_index = orden['id']
+
+    return higher_index + 1
+
+def detalles_ordenes(request, id):
+    ordenes = cargar_ordenes()
+
+    if request.method == 'GET':
+        orden = next((o for o in ordenes if o['id'] == id), None)
+
+        if orden is None:
+            return JsonResponse({"mensaje" : "No se encontró la Orden"}, safe=False, status=404)
+
+        return JsonResponse(orden, safe=False, status=200)
+
+
+    elif request.method == 'PATCH':
+
+        orden = next((o for o in ordenes if o['id'] == id), None)
+
+        if orden is None:
+            return JsonResponse({"mensaje" : "Orden No encontrada"}, safe=False, status=404)
+
+        orden_actualizada = json.loads(request.body.decode('utf-8'))
+        orden.update(orden_actualizada)
+        guardar_ordenes(orden)
+        return JsonResponse({"mensaje" : "Orden actualizada correctamente"}, safe=False, status=200)
+
+
+    elif request.method == 'DELETE':
+
+        orden = next((o for o in ordenes if o['id'] == id), None)
+
+        if orden is None:
+            return JsonResponse({"mensaje": "Orden no encontrada"}, safe=False, status=404)
+
+        # If the product exists, then create a new array excluding the product of the {id}
+        new_orden_list = [u for u in ordenes if u['id'] != id]
+        guardar_ordenes(new_orden_list)
+        return JsonResponse({"mensaje" : "Orden Eliminada Correctamente"}, safe=False, status=204)
+
+
